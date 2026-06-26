b8366d4490818906278726a09f2c7042c9db1153
Andrey Sitnik <andrey@sitnik.es>
2026-06-26 22:01:38 +0000

    Remove package.json since Shelly JS is not normal JS

--- wall-switch.js
+++ wall-switch.js
@@ -2,10 +2,10 @@
 // short press (<1s)          -> toggle Zigbee group
 // long-short-long-short-long -> enable WiFi
 
-const GROUP_ID = 1 // Zigbee group ID from Z2M
-const MAX_PRESS_MS = 1000 // Long press timing
-const SEQUENCE_GAP_MS = 3000 // Pattern timing
-const WIFI_PATTERN = 'LSLSL' // WiFi command pattern (L - long, S - short)
+let GROUP_ID = 1 // Zigbee group ID from Z2M
+let MAX_PRESS_MS = 1000 // Long press timing
+let SEQUENCE_GAP_MS = 3000 // Pattern timing
+let WIFI_PATTERN = 'LSLSL' // WiFi command pattern (L - long, S - short)
 
 let longReached = false
 let longTimer = null
@@ -22,7 +22,7 @@ function toggleLight() {
       cmd: 2, // toggle
       timeout_ms: 1000 // ZCL answer response
     },
-    (res, err, msg) => {
+    function (res, err, msg) {
       if (err) print('Group ZCL err:', err, msg)
     }
   )
@@ -32,7 +32,7 @@ function enableWifi() {
   Shelly.call(
     'WiFi.SetConfig',
     { config: { sta: { enable: true } } },
-    (res, err, msg) => {
+    function (res, err, msg) {
       if (err) {
         print('WiFi enable err:', err, msg)
       } else {
@@ -63,13 +63,13 @@ function registerPress(c) {
   }
 }
 
-Shelly.addEventHandler(e => {
+Shelly.addEventHandler(function (e) {
   if (e.component !== 'input:0') return
 
   if (e.info.event === 'btn_down') {
     longReached = false
     if (longTimer !== null) Timer.clear(longTimer)
-    longTimer = Timer.set(MAX_PRESS_MS, false, () => {
+    longTimer = Timer.set(MAX_PRESS_MS, false, function () {
       longReached = true
       longTimer = null
     })
